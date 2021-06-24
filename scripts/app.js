4780211fdd199591080ca6f5022e6c99c217e6f5
NicoleLund <78176837+NicoleLund@users.noreply.github.com>
2021-06-24 10:24:37 -0700

    Confirm JSON loading

--- scripts/app.js
+++ scripts/app.js
@@ -27,13 +27,36 @@ HINT 2
 // Load samples.json
 d3.json("data/samples.json").then((importedData) => {
    var data = importedData;
-   var metadata = data.metadata;
+   console.log("------data-------");
    console.log(data);
+
+   var metadata = data.metadata;
+   console.log("------metadata-------");
    console.log(metadata);
 
-   var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-   var result = resultArray[0];
-   console.log(result);
+   var names = data.names;
+   console.log("------names-------");
+   console.log(names);
+
+   var samples = data.samples;
+   console.log("------samples-------");
+   console.log(samples);
+
+   // Review data for id=945
+   sample = 945
+   console.log("------Review data for id=495-------");
+
+   var metadataResultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+   console.log("------metadata-------");
+   console.log(metadataResultArray[0]);
+
+   var namesResultArray = names.filter(sampleObj => sampleObj.id == sample);
+   console.log("------names-------");
+   console.log(namesResultArray[0]);
+
+   var samplesResultArray = samples.filter(sampleObj => sampleObj.id == sample);
+   console.log("------samples-------");
+   console.log(samplesResultArray[0]);
 });
 
 // Create bar chart in "bar" section
